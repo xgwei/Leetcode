4f4e4529ad094116a2725b69d9df3cec33fabc7f
xgwei <xg.wei@yahoo.com>
2018-05-26 15:46:29 +0800

    Create 最长上升子序列.py

---
+++ 最长上升子序列.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0:
+            return 0
+        if len(nums)==1:
+            return 1
+        a=nums
+        dp=[ 0 for i in range(len(nums))]
+        
+        for i in range(len(nums)):
+            Max = 0;
+            for j in range(i):
+                if (a[i] > a[j]):
+                    Max = max(Max, dp[j])
+            dp[i] = Max + 1;
+        Max = 0;
+        for i in range(len(nums)):
+            if (dp[i] > Max):
+                Max = dp[i]
+        return Max
